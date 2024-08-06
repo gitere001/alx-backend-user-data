4e24bab64ac94ec1567dd3f401d98c0842c0eefa
James gitere <sasmuypwa2019.com>
2024-08-06 19:09:29 +0300

    updated

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ Basic Authentication class	"""
 from api.v1.auth.auth import Auth
+from typing import Tuple
 
 
 class BasicAuth(Auth):
@@ -27,3 +28,20 @@ class BasicAuth(Auth):
         if not authorization_header.startswith('Basic '):
             return None
         return authorization_header.split("Basic ")[1]
+
+    def extract_user_credentials(self,
+                                 decoded_base64_authorization_header:
+                                     str) -> Tuple[str, str]:
+        """ Extracts user email and password from the Base64 decoded string """
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+
+        # Split the string into email and password
+        credentials = decoded_base64_authorization_header.split(':', 1)
+
+        # Return the extracted email and password
+        return credentials[0], credentials[1]
