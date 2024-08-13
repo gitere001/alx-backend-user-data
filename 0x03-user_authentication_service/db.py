31398675ca829d67ca0645deecadf4fd7a1d109c
James gitere <sasmuypwa2019.com>
2024-08-13 17:59:54 +0300

    updated

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -32,10 +32,14 @@ class DB:
 
     def add_user(self, email: str, hashed_password: str) -> User:
         """Add a new user to the database"""
-        user = User(email=email, hashed_password=hashed_password)
-        self._session.add(user)
-        self._session.commit()
-        return user
+        try:
+            user = User(email=email, hashed_password=hashed_password)
+            self._session.add(user)
+            self._session.commit()
+            return user
+        except Exception:
+            self._session.rollback()
+            return None
 
     def find_user_by(self, **kwargs) -> Union[User, None]:
         """finds a user by a given criteria"""
